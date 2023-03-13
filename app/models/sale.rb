2452ddc3d6573bb69d9cfc01c9fa614c2f656545
JimHwkins <howlarrior@gmail.com>
2023-03-13 16:12:26 -0300

    Add sales status to admin/sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,17 @@
 class Sale < ApplicationRecord
+  def self.active
+    where("sales.start_date <= ? AND sales.end_date >= ?", Date.current, Date.current)
+  end
+
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
 end
