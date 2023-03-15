110ca788d62d1c7b4fa7dd9e6512dfede7656e4d
JimHwkins <howlarrior@gmail.com>
2023-03-15 14:16:19 -0300

    Add letter_opener to check e-mail receipt function

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,9 +74,9 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.perform_deliveries = true
+config.action_mailer.perform_deliveries = true
 
-config.action_mailer.delivery_method = :smtp
+config.action_mailer.delivery_method = :letter_opener
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',  
   port:                 587,                 
