8df8e21d536918a06aa869d3f021e3e5b331da04
JimHwkins <howlarrior@gmail.com>
2023-03-14 22:09:57 -0300

    Add Cypress test for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,9 @@
+describe("example to-do app", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+});
