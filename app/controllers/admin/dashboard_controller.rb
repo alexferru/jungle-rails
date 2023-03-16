a13e189978aa710f7fd0795cef46fd5669a8d84d
JimHwkins <howlarrior@gmail.com>
2023-03-16 01:12:37 -0300

    Fix some styling and add sales count to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,5 +4,6 @@ class Admin::DashboardController < ApplicationController
   def show
     @product_count = Product.count
     @category_count = Category.count
+    @sale_count = Sale.count
   end
 end
